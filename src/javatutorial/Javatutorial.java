36c60a083b0bce186e165d67c2f5539f8ef82bb5
desan <desan@10.10.17.14>
2022-01-25 13:55:40 -0500

    INITIAL COMMIT

---
+++ src/javatutorial/Javatutorial.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javatutorial;
+
+/**
+ *
+ * @author desan
+ */
+public class Javatutorial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Welcome to Git Programming");
+
+    }
+    
+}
